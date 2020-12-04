c0daf13691546e2c0d53ce80570faf80ea29997c
DiegoMejia12 <damcrayg@gmail.com>
2020-12-04 00:27:46 -0500

    Correciono de Erorres de peticiones a  APi

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def upload_s3(endpoint, Body, nameFile):
     now = str(datetime.now())[0:16]
 
     s3.put_object(Body=Body, Bucket='produccion',
-                  Key="{date}/prueba/{endpoint}/{namefile}".format(date=now, endpoint=endpoint, namefile=nameFile))
+                  Key="{date}/{endpoint}/{namefile}".format(date=now, endpoint=endpoint, namefile=nameFile))
 
 
 ##s cadena con la informacion json de la descripcion de peliculas)
